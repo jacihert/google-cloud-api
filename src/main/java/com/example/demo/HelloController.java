6c9ce03fd8a0f4e6295b3838c4d7e4a20c43d44c
jacintha <jacintha.h@gmail.com>
2022-06-22 11:43:42 +0200

    updated cross Origin

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class HelloController {
     @GetMapping("/hello")
