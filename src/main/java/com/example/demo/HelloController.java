c101f38a76dbc5fb971d358bf0b681447eb69751
jacintha <jacintha.h@gmail.com>
2022-06-22 12:15:13 +0200

    fixed code

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class HelloController {
@@ -11,7 +14,11 @@ public class HelloController {
     @Autowired
     HelloRepository repo;
     @GetMapping("/hello")
-    public String sayHello() {
-        return "Guten Tag. Wie geht es dir? Mein name ist Jacintha. ";
+    public List<Hello> sayHello() {
+        return repo.findAll();
     }
+
+//    public String sayHello() {
+//        return "Guten Tag. Wie geht es dir? Mein name ist Jacintha. ";
+//    }
 }
