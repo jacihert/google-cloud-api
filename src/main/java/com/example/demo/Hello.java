bd37c7f81b20aa8adef3eef6ecd638301e721e45
jacintha <jacintha.h@gmail.com>
2022-06-22 13:38:02 +0200

    added getters & setters

--- src/main/java/com/example/demo/Hello.java
+++ src/main/java/com/example/demo/Hello.java
@@ -10,4 +10,20 @@ public class Hello {
     @Id
     private int id;
     private String greeting;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
 }
