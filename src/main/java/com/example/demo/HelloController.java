0f2b5de3033b38aeba55c1b5d0dab07e93c0a9d6
jacintha <jacintha.h@gmail.com>
2022-06-22 12:04:24 +0200

    included autowired

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class HelloController {
+
+    @Autowired
+    HelloRepository repo;
     @GetMapping("/hello")
     public String sayHello() {
         return "Guten Tag. Wie geht es dir? Mein name ist Jacintha. ";
